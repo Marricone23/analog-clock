1cb969b58bf1b37209ad05450b95f91d74ea2a8a
Marricone23 <68504252+Marricone23@users.noreply.github.com>
2022-03-02 16:01:11 +0300

    update script

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ let hr = document.querySelector('#hr');
 let sec = document.querySelector('#sec');
 let min = document.querySelector('#min');
 
-function clock() {
-    setInterval(()=>{
-let day = new Date();
-        let hh =day.getHours()*30;
-        let sc =day.getSeconds()*deg;
-        let mn =day.getMinutes()*deg;
+function clock() { setInterval(()=> {
+        let day = new Date();
+        let hh = day.getHours()*30;
+        let sc = day.getSeconds()*deg;
+        let mn = day.getMinutes()*deg;
 
         hr.style.transform = `rotateZ(${(hh)+(mn/12)}deg)`;
         sec.style.transform = `rotateZ(${sc}deg)`;
